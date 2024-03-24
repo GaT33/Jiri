e898f9e3bdb14c2b86ec2dbbb53d661e240f5193
GrPK <alexandru.contac@student.tuiasi.ro>
2024-03-24 18:17:40 +0200

    Cleaned up the Main Class

--- src/main/java/ro/tuiasi/Main.java
+++ src/main/java/ro/tuiasi/Main.java
@@ -4,8 +4,6 @@ package ro.tuiasi;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Here we go.");
     }
 }
