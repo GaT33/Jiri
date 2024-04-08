a0a908e0607cfde252199ac526b7df71d7d38f1a
GrPK <alexandru.contac@student.tuiasi.ro>
2024-04-08 12:12:55 +0300

    Testing Better Audio Quality

--- src/main/java/ro/tuiasi/AudioRecord.java
+++ src/main/java/ro/tuiasi/AudioRecord.java
@@ -25,7 +25,7 @@ public class AudioRecord {
     AudioFormat getAudioFormat() {
         float sampleRate = 16000;
         int sampleSizeInBits = 8;
-        int channels = 2;
+        int channels = 1;
         boolean signed = true;
         boolean bigEndian = true;
         AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits,channels, signed, bigEndian);
