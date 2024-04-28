d7be2f387e75abf800ab40147819be4f17e67514
GrPK <alexandru.contac@student.tuiasi.ro>
2024-04-28 19:47:13 +0300

    Clean-up the main, prepare to unite the code.

--- src/main/java/ro/tuiasi/Main.java
+++ src/main/java/ro/tuiasi/Main.java
@@ -1,68 +1,8 @@
 package ro.tuiasi;
 
-import java.util.ArrayList;
-import java.util.List;
-import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-import com.theokanning.openai.completion.chat.ChatMessage;
-import com.theokanning.openai.service.*;
-import com.theokanning.openai.client.*;
-import com.theokanning.openai.completion.*;
-import com.theokanning.openai.completion.chat.*;
-import com.theokanning.openai.service.FunctionExecutor;
-import com.theokanning.openai.service.OpenAiService;
-
-import io.reactivex.Flowable;
-
-import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
 public class Main {
     public static void main(String... args) {
-      //implementare conexiunea API + streaming (consola) mai trebuie speech ul
-        OpenAiService service = new OpenAiService("cheie Token");
 
-        List<ChatMessage> messages = new ArrayList<>();
-        ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), "You are an assistant.");
-        messages.add(systemMessage);
-
-        System.out.print("First Query: ");
-        Scanner scanner = new Scanner(System.in);
-        ChatMessage firstMsg = new ChatMessage(ChatMessageRole.USER.value(), scanner.nextLine());
-        messages.add(firstMsg);
-
-        while (true) {
-            ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
-                    .builder()
-                    .model("gpt-3.5-turbo-0613")
-                    .messages(messages)
-                    .n(1)
-                    .maxTokens(256)
-                    .build();
-            Flowable<ChatCompletionChunk> flowable = service.streamChatCompletion(chatCompletionRequest);
-
-            AtomicBoolean isFirst = new AtomicBoolean(true);
-            ChatMessage chatMessage = service.mapStreamToAccumulator(flowable)
-                    .doOnNext(accumulator -> {
-                        if (isFirst.getAndSet(false)) {
-                            System.out.print("Response: ");
-                        }
-                        if (accumulator.getMessageChunk().getContent() != null) {
-                            System.out.print(accumulator.getMessageChunk().getContent());
-                        }
-                    })
-                    .doOnComplete(System.out::println)
-                    .lastElement()
-                    .blockingGet()
-                    .getAccumulatedMessage();
-            messages.add(chatMessage); // don't forget to update the conversation with the latest response
-
-            System.out.print("Next Query: ");
-            String nextLine = scanner.nextLine();
-            if (nextLine.equalsIgnoreCase("exit")) {
-                scanner.close();
-                break;
-            }
-            messages.add(new ChatMessage(ChatMessageRole.USER.value(), nextLine));
-        }
     }
 
 }
