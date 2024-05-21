2cf349c957e48ac0719755468bb4085e8704c9c0
GrPK <alexandru.contac@student.tuiasi.ro>
2024-05-22 01:43:30 +0300

    Merged all the branches. We have half the Interface!

--- src/main/java/ro/tuiasi/Main.java
+++ src/main/java/ro/tuiasi/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 
 public class Main {
 
-    public static OpenAiService service = new OpenAiService("sk-proj-R1uNHZaMvPUi75XJipmbT3BlbkFJkkMQ0e0XQN0IJHokkU97");
+    public static OpenAiService service = new OpenAiService("");
 
     public static void main(String[] args) {
 
